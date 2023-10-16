a9820f57bd4d1111ea6841a1473be13c41c9bc6b
marcosjr1307 <mesqmamjr@gmail.com>
2023-10-16 13:32:44 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+function change(){
+    document.getElementById("title").style.color= "lightgreen";
+}
+
+function dischange(){
+    document.getElementById("title").style.color= "black";
+}
+
+var variavel = 1.1;
+console.log('variavel = '+variavel);
+console.log(typeof variavel);
+
+variavel = "Olá mundo";
+console.log('variavel = '+variavel);
+console.log(typeof variavel);
+
+variavel = true;
+console.log('variavel = '+variavel);
+console.log(typeof variavel);
+
+console.log('Minha mãe sempre dizia : "Volte já para casa menino"');
+
+if(variavel){
+    console.log("Igual");
+}
