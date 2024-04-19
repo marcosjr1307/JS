18a61770bdf889b6df8e57ed2c31444e7c4ca995
marcosjr1307 <mesqmamjr@gmail.com>
2024-04-19 11:01:59 -0300

    cookie

---
+++ 2024/PHP/sessionCookie/exemploRemoveCookie.php
@@ -0,0 +1,6 @@
+<?php
+    setcookie('InfoUser-Nome', 0, time()-3600 , "/");
+    setcookie('InfoUser-Idade', 0, time()-3600 , "/");
+    setcookie('InfoUser-contaVisitas', 0, time()-3600 , "/");
+    setcookie('InfoUser-Cor', 0, time()-3600 , "/");
+?>
--- 2024/PHP/sessionCookie/exemploUsaCookie.php
+++ 2024/PHP/sessionCookie/exemploUsaCookie.php
@@ -4,12 +4,33 @@
     }else{
         $nome = $_COOKIE["InfoUser-Nome"];
         $idade = $_COOKIE["InfoUser-Idade"];
-        $contaVisitas = $_COOKIE["InforUser-contaVisitas"];
+        $contaVisitas = $_COOKIE["InfoUser-contaVisitas"];
 
         if($idade < 18){
             $msg = "Você não tem idade para acessar esse site!";
         }else{
             $cor = $_COOKIE["InfoUser-Cor"];
+
+            if($contaVisitas>0){
+                $msg = "Olá ". $nome . "! Bem vindo de volta ao nosso site...";
+            }else{
+                $msg = "Olá ". $nome . "! Bem vindo ao nosso site...";
+                $contaVisitas++;
+                setcookie("InfoUser-contaVisitas", $contaVisitas, time()+3600, "/");
+            }
         }
+
     }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body style= "background-color: <?=$cor?>">
+    <?=$msg?>
+</body>
+</html>
