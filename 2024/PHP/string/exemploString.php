4db3300208fc0a66a7bae17dcd0230e8d3e988a6
marcosjr1307 <mesqmamjr@gmail.com>
2024-05-03 11:07:46 -0300

    string

---
+++ 2024/PHP/string/exemploString.php
@@ -0,0 +1,12 @@
+<?
+    $texto = " cotil - unicamp ";
+
+    //trim - Retira o espaço no início e final de um string
+    echo "-" . trim($texto) . "-" . "<br>";
+
+    //Retira o espaço em branco (ou outros caracteres) do início da string
+    echo "-" . ltrim($texto) . "-" . "<br>";
+
+    //rtrim - Retira espaços em branco (ou outros caracteres) do fim da string
+    echo "-" . rtrim($texto) . "-" . "<br>";
+?>
